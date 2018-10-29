1ac09bd69d787c259cb977eda346e12a96b74068
Getty Ritter <gettylefou@gmail.com>
2018-10-29 16:07:28 -0700

    remove language extension: nobody needs RankNTypes for this

--- Lens/Yocto.hs
+++ Lens/Yocto.hs
@@ -1,13 +1,11 @@
-{-# LANGUAGE RankNTypes #-}
-
 module Lens.Yocto ( Lens, lens ) where
 
 -- | This is the type alias for a @Lens@ in other @lens@
 --   packages. It is compatible with any other package that can
 --   use the same family of lenses, including @microlens@ or
 --   @lens-family-core@.
-type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+type Lens f s t a b = (a -> f b) -> s -> f t
 
 -- | This is a function to build a 'Lens' from a getter and a setter.
-lens :: (s -> a) -> (b -> s -> t) -> Lens s t a b
+lens :: Functor f => (s -> a) -> (b -> s -> t) -> Lens f s t a b
 lens get set f a = (`set` a) `fmap` f (get a)
